b2ba1879be0d3b4e254ff5dee742e07de02398c8
somename <someemail>
2016-10-05 16:40:18 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -5,11 +5,11 @@ public class Spreadsheet {
 
 	Map<String,Integer> sheet = new HashMap<String,Integer>();
 	
-	public int get(String cell) {
+	public String get(String cell) {
 		return sheet.get(cell);
 	}
 	
-	public void set(String cell, int value) {
+	public void set(String cell, String value) {
 		sheet.put(cell, value);
 	}
 	
