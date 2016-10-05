55732c61b671766ca61b68966bfe03055e50346e
Itir Karac <karacitir@gmail.com>
2016-10-05 13:44:59 +0300

    Project template created

---
+++ src/Spreadsheet.java
@@ -0,0 +1,18 @@
+
+public class Spreadsheet {
+
+	public String get(String cell) {
+		// to be implemented
+		return null;
+	}
+	
+	public void set(String cell, String value) {
+		// to be implemented
+	}
+	
+	public String evaluate(String cell) {
+		// to be implemented
+		return null;
+	}
+	
+}
