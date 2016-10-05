7c3c1a1d2ff22109b0b20847bdb10e7b417ab9a0
somename <someemail>
2016-10-05 16:41:44 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -10,18 +10,13 @@ public class Spreadsheet {
 	
 	public void set(String cell, String value) {
 		if (value.length() > 0) {
-			if (value.charAt(0) == '-' || 
-					value.charAt(0) >= '0' || value.charAt(0) <= '9') {
-				if (value.length() == 1) {
+			boolean isInteger = false,
+			for (int i = 1; i < value.length(); i++) {
+				char c = value.charAt(i);
+				if (i == 0 && )
+				if (c < '0' || c > '9') {
 					value = "#Error";
-				} else {
-					for (int i = 1; i < value.length(); i++) {
-						char c = value.charAt(i);
-						if (c < '0' || c > '9') {
-							value = "#Error";
-							break;
-						}
-					}
+					break;
 				}
 			}
 		}
