dfd9c8de66a89a7657e16189ef413facdf4f52c3
somename <someemail>
2016-10-05 18:50:24 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -78,4 +78,11 @@ public class SpreadsheetTest {
 		assertEquals("#Error", evaluatedValue);
 	}
 
+	@Test
+	public void test_set_and_evaluate_A1_equals_a_string_without_trailing_quote() {
+		s.set("A1", "='a string");
+		String evaluatedValue = s.evaluate("A1");
+		assertEquals("#Error", evaluatedValue);
+	}
+
 }
