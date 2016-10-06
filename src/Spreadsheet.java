063efdb8d352717da8623445f137b3d9b57217c9
somename <someemail>
2016-10-06 15:20:43 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,32 +1,27 @@
 
 public class Spreadsheet {
-	
-		
-	public Spreadsheet(){
-		
-			
-	}
-	
-	String cellVar="0";
-	
-	
-	
 
-	public String get(){
+	public Spreadsheet() {
+
+	}
+
+	String cellVar = "0";
+
+	public String get() {
 		// to be implemented
 		return cellVar;
-		
+
 	}
-	
+
 	public void set(String value) {
 		// to be implemented
 		cellVar = value;
-		
+
 	}
-	
+
 	public String evaluate(String cell) {
 		// to be implemented
 		return null;
 	}
-	
+
 }
