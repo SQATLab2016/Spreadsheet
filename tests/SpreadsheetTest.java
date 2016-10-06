7318db67689ae609f6e0e0b5e8085fa1c3729359
somename <someemail>
2016-10-06 16:27:00 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -5,11 +5,21 @@ import org.junit.Test;
 
 
 public class SpreadsheetTest {
+	
+	Spreadsheet sheet;
+	
+	@Before
+	public void init() {
+		sheet = new Spreadsheet();
+		sheet.init();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void testCreateCellA1() {
+		sheet.set("A1", "1");
+		String s = sheet.get("A1");
+		assertEquals("1", s);
+				
 	}
 
 }
