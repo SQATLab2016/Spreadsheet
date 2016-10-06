39cc8a25b530447db1f042814bb108068e18a5b7
kuzdoan <kuzdoan@STUPID-PC710.stnet.local>
2016-10-06 17:00:44 +0300

    Project to be continued

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,18 +1,93 @@
+import java.util.HashMap;
 
 public class Spreadsheet {
-
+	HashMap<String, String> sheet = new HashMap<String, String>();
+	
+	
 	public String get(String cell) {
-		// to be implemented
-		return null;
+		String value = sheet.get(cell);
+		
+		/*if(isInteger(value))
+			return value;
+		else if(isCellString(value))
+			return value;*/
+		return value;
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		sheet.put(cell, value);
 	}
 	
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
+		String value = sheet.get(cell);
+		
+		if(isInteger(value))
+			return value;
+		
+		else if(isCellString(value)){
+			// Remove quotes
+			value = value.substring(1,value.length()-1);
+			return value;
+		}
+		
+		else if (beginsEquals(value)){
+			// Remove the equals sign.
+			value = value.substring(1);
+			if(isInteger(value))
+				return value;
+			
+			else if(isCellString(value)){
+				// Remove quotes.
+				value = value.substring(0,value.length()-1);
+				return value;
+			}
+			
+			// If reference to a cell.
+			else if(isCell(value)){
+				String refCell = evaluate("=" + value);
+				if(refCell.equals("="+value)) // Circular?
+					return "#Circular";
+				return evaluate("="+value);
+			}
+		}
+		return  "#Error";
 	}
 	
+	public boolean isInteger(String string) {
+    try {
+        Integer.valueOf(string);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
+    }
+	}
+	
+	// Can only check cells with a single letter.
+	public boolean isCell(String str){
+		char letter = str.charAt(0);
+		if(letter<65 || letter > 90)
+			return false;
+		String number = str.substring(1);
+		// Does not check positive-negative integers.
+		if(!isInteger(number))
+			return false;
+		return true;
+	}
+	
+	public boolean isCellString(String str){
+		if(str.length()<=2)
+			return false;
+		char first = str.charAt(0);
+		char last = str.charAt(str.length()-1);
+		
+		if(first=='\'' && last=='\'')
+				return true;
+		return false;
+	}
+	
+	public boolean beginsEquals(String str){
+		if (str.length()>0 && str.charAt(0)== '=')
+			return true;
+		return false;
+	}
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -5,11 +5,74 @@ import org.junit.Test;
 
 
 public class SpreadsheetTest {
-
+	Spreadsheet sheet = new Spreadsheet();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsInteger_213214_true(){	
+		assertTrue(sheet.isInteger("213214"));
+	}
+	
+	@Test
+	public void testIsInteger_A1_false(){	
+		assertFalse(sheet.isInteger("A1"));
+	}
+	
+	@Test
+	public void testIsCell_231_false(){
+		assertFalse(sheet.isCell("231"));
+	}
+	
+	@Test 
+	public void testIsCell_A32A_true(){
+		assertFalse(sheet.isCell("A32A"));
+	}
+	
+	@Test 
+	public void testIsCell_X23_false(){
+		assertTrue(sheet.isCell("X23"));
+	}
+	
+	@Test
+	public void testIsCellString_a_string(){
+		assertFalse(sheet.isCellString("a string"));
+	}
+	
+	@Test
+	public void testIsCellString_q_a_string_q(){
+		assertTrue(sheet.isCellString("'a string'"));
+	}
+	
+	@Test 
+	public void testSet_A2_321(){
+		sheet.set("A2", "321");
 		
+		String value = sheet.get("A2");
+		
+		assertEquals("321", value);
 	}
 
+	@Test
+	public void testEvaluate_Integer(){
+		sheet.set("A1", "546");
+		
+		String value = sheet.evaluate("A1");
+		
+		assertEquals("546", value);
+	}
+	
+	@Test
+	public void testEvaluate_String(){
+		sheet.set("B2", "'a string'");
+		
+		String value = sheet.evaluate("B2");
+		
+		assertEquals("a string", value);
+	}
+	
+	@Test
+	public void testEvaluate_Integer_With_Formula(){
+		sheet.set("B2", "=12345");
+		
+		String value = sheet.evaluate("B2");
+	}
 }
