d65e485adc72291ca269d1e69e46282206b620c9
somename <someemail>
2016-10-06 16:28:58 +0300

    besouro automatic message

--- src/Cell.java
+++ src/Cell.java
@@ -4,7 +4,10 @@ public class Cell  {
 	public String value;
 	
 	
-	public String getValue(){ return this.value;}
+	public String getValue(){ 
+		if (parseInt(value, 10).isNaN) return this.value;
+		else return parseInt(value,10);
+	}
 
 
 	public void setValue(String value) {
