20bd065e2ab19ecfad493a29b1b3911dc68f79ec
somename <someemail>
2016-10-06 16:01:36 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 
 public class Spreadsheet {
-	ArrayList<ArrayList> sheetMatrix = new ArrayList(26);
+	ArrayList<ArrayList> sheetMatrix = new ArrayList(27);
+	char[] alphabet = new char[26];
+	
+	public Spreadsheet() {
+		createAlphabetArray();
+	}
+
 	public String get(String cell) {
 		// to be implemented
 		return null;
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		char row = cell.charAt(0);
+		char col = cell.charAt(1);
+		
 	}
 	
 	public String evaluate(String cell) {
@@ -16,4 +24,11 @@ public class Spreadsheet {
 		return null;
 	}
 	
+	private void createAlphabetArray(){
+           for(char ch = 'a'; ch <= 'z'; ++ch)// fills alphabet array with the alphabet
+           {
+               alphabet[ch-'a']=ch;
+           } 
+    }
+
 }
