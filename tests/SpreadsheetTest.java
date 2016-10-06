f171689b7a9eb71f2cd100e8fe83c3694a4f19b5
somename <someemail>
2016-10-06 15:13:47 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,9 +7,14 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void test_setAndGet_1() {
+		//Arrange
+		Spreadsheet sheet = new Spreadsheet();
+		//Act
+		sheet.set("A1", "1");
+		int num = sheet.get("A1")
+		//Assert
+		assertEquals(1, num);
 	}
 
 }
