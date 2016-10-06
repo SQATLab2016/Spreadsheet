e0beb0bd9b164247d101e1d472536fea978c67af
somename <someemail>
2016-10-06 14:45:03 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -11,5 +11,12 @@ public class SpreadsheetTest {
 		fail("Not yet implemented");
 		
 	}
+	
+	@Test
+	public void GetCellValue(){
+		Cell Cell = new Cell();
+		Cell.value="1";
+		assertEquals("",1,Cell.getValue());
+	}
 
 }
