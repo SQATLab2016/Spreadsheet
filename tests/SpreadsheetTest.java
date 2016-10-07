f318f91bb405c86990f9605bf6efadea5d399482
somename <someemail>
2016-10-07 16:34:08 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -6,11 +6,16 @@ import org.junit.Test;
 
 public class SpreadsheetTest {
 
+	private Spreadsheet sheet = new Spreadsheet();
 	@Test
 	public void test_setCellToValueStringA1_1() {
-		Spreadsheet sheet = new Spreadsheet();
+		
 		sheet.set("A1", "1");
 		assertEquals("1", sheet.get("A1"));
 	}
 
+	public void test_evaluateCorrectlyFormattedCellInteger() {
+		
+		
+	}
 }
