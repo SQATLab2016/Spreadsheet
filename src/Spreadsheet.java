e3755df1c2cf89d647e99da5b6dea3f57d09daac
somename <someemail>
2016-10-07 16:41:52 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,13 +1,21 @@
+import java.util.*;
 
 public class Spreadsheet {
+	
+	private HashMap<String, String> cells;
+	
+	public Spreadsheet(){
+		cells = new HashMap<String, String>();
+	}
 
 	public String get(String cell) {
 		// to be implemented
-		return null;
+		return cells.get(cell);
 	}
 	
 	public void set(String cell, String value) {
 		// to be implemented
+		cells.put(cell, value);
 	}
 	
 	public String evaluate(String cell) {
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,9 +7,19 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_SetAndGet() {
+		Spreadsheet n = new Spreadsheet();
+		n.set("A1", "2");
 		
+		assertEquals("Should return 2", "2",  n.get("A1"));
+		
+	}
+	@Test
+	public void test_NumberHandlingNegative() {
+		Spreadsheet n = new Spreadsheet();
+		n.set("A1", "-1");
+		
+		assertEquals("Evaluation is not correct", "-1", n.evaluate("A1"););
 	}
 
 }
