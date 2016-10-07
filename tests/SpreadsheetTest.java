7ecd83dfca27ec4d686a66ec16adf987aee9c8e8
somename <someemail>
2016-10-07 16:18:13 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -17,22 +17,22 @@ public class SpreadsheetTest {
 	@Test
 	public void test2() {
 		test.set("A2", "-1");
-	    assertTrue(test.get("A2").equals("-1"));	
+	    assertTrue(test.evaluate("A2").equals("-1"));	
 	}
 	@Test
 	public void test3() {
 		test.set("A3", "5A");
-	    assertTrue(test.get("A3").equals("#Error"));	
+	    assertTrue(test.evaluate("A3").equals("#Error"));	
 	}
 	@Test
 	public void test4() {
 		test.set("A1", "’a string’");
-	    assertTrue(test.get("A1").equals("a string"));	
+	    assertTrue(test.evaluate("A1").equals("a string"));	
 	}
 	@Test
 	public void test5() {
 		test.set("A1", "’a string");
-	    assertTrue(test.get("A1").equals("#Error"));	
+	    assertTrue(test.evaluate("A1").equals("#Error"));	
 	}
 	
 	
