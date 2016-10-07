8f9ca510f004d960f30f8b5139fa1f30566d7943
somename <someemail>
2016-10-07 16:31:42 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test_setCellToValueA1_1() {
+	public void test_setCellToValueStringA1_1() {
 		Spreadsheet sheet = new Spreadsheet();
 		sheet.set("A1", "1");
 		assertEquals(1, sheet.get("A1"));
