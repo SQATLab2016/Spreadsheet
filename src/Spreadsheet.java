472ea151eef60cb016e40e10542d738450760db5
somename <someemail>
2016-10-07 16:17:32 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -94,6 +94,11 @@ public class Spreadsheet {
 						return "#Error";
 					}
 				
+				// next character is a digit.
+				} else if(Character.isDigit(c[i+1])) {
+				
+				}
+					
 				} else {
 					return "#Error";
 				}
