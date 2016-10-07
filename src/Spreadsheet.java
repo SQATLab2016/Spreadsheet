7758720dc6e2dd5088e4082b1d88cc8067991b48
mikkoyli <mikkoyli@STUPID-PC722.stnet.local>
2016-10-07 16:36:17 +0300

    Some code written to this

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,13 +1,16 @@
+import java.util.HashMap;
 
 public class Spreadsheet {
+	
+	HashMap<String, String> cells = new HashMap<String, String>();
 
 	public String get(String cell) {
-		// to be implemented
-		return null;
+		String value = cells.get(cell);
+		return value;
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		cells.put(cell, value);
 	}
 	
 	public String evaluate(String cell) {
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 
 
 public class SpreadsheetTest {
+	
+	Spreadsheet ss = new Spreadsheet();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void test_set_A1_1() {
+		ss.set("A1", "1");
+		assertEquals("1", ss.get("A1"));
 	}
 
 }
