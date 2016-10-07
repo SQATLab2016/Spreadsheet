c2c85747c5a94a315feb0c6e907695f655b99649
somename <someemail>
2016-10-07 14:21:19 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,6 +1,10 @@
+import java.util.*;
 
 public class Spreadsheet {
 
+	Map <String, String> map = new HashMap<String, String>();
+	
+	
 	public String get(String cell) {
 		// to be implemented
 		return null;
