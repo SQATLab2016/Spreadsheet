aca217e9fe39e18002a42c5d693407bd6215b8f9
somename <someemail>
2016-10-07 16:41:59 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -64,7 +64,8 @@ public class Spreadsheet {
 						   }else{
 							   return "#Error"; // 7
 						   }
-					   }else if(cells.contains(cells.get(cells.lastIndexOf(cell)+1).substring(1))){
+					   }else if(cells.contains(cells.get(cells.lastIndexOf(cell)+1).substring(1))){ // 8 =A1
+						   
 						   return cells.get(cells.lastIndexOf(cells.get(cells.lastIndexOf(cell)+1).substring(1))+1);
 					   }
 				}else{
