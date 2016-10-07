1888284369f7a304807752d8aa1749ad257ca484
somename <someemail>
2016-10-07 14:43:43 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,13 +1,14 @@
 
 public class Spreadsheet {
 
+
 	public String get(String cell) {
 		// to be implemented
 		return null;
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+	   
 	}
 	
 	public String evaluate(String cell) {
