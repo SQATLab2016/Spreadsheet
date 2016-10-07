cca413ec44881edde18ca48484e27d840a3eb3e3
somename <someemail>
2016-10-07 16:35:07 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -130,7 +130,7 @@ public class Spreadsheet {
 				if(digitCount != c.length - 2) {
 					return "#Error";
 				} else {
-					return value;
+					return value.substring(startPos, endPos);
 				}
 			}
 			
