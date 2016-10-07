570632916b09eef9949b03c867abc54494916add
somename <someemail>
2016-10-07 15:31:40 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -45,7 +45,7 @@ public class Spreadsheet {
 			}
 			
 			// if there is a '=' character
-			// the subsequent char is either '-', '\'' or digit.
+			// the subsequent char can only be '-', '\'' or digit.
 			if(c[i] == '=') {
 				if(c[i+1] == '-' || c[i+1] == '\'' || Character.isDigit(c[i+1])) {
 					
