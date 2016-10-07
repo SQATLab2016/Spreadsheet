9c1d1e3b422e2b96582e732fd4bcff5db1bd8545
somename <someemail>
2016-10-07 16:22:35 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -9,8 +9,7 @@ public class SpreadsheetTest {
 	@Test
 	public void test_setCellToValueA1_1() {
 		Spreadsheet sheet = new Spreadsheet();
-		sheet.set("A1", "1");
-		
+		sheet.set("A1", "1");		
 	}
 
 }
