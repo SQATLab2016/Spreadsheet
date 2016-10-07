517ef7b2d4da673cb18c587754ee27ea94212c27
somename <someemail>
2016-10-07 16:44:10 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -31,6 +31,7 @@ public class SpreadsheetTest {
 	@Test
 	public void test_NumberHandlingSigned() {
 		Spreadsheet n = new Spreadsheet();
+		
 		n.set("A1", "1");
 		
 		assertEquals("Evaluation is not correct", "1", n.evaluate("A1"));
