077c0b31e565b099c3932d791837b810dd5606dd
zzu <zzu@STUPID-PC702.stnet.local>
2016-10-07 17:36:42 +0300

    Finish first 10 rules.

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,18 +1,54 @@
+import java.util.HashMap;
 
 public class Spreadsheet {
-
+	
+	private HashMap<String,String> hashmap;
+	
+	public Spreadsheet(){
+		hashmap = new HashMap<String,String>();
+	}
+	
 	public String get(String cell) {
-		// to be implemented
-		return null;
+		return hashmap.get(cell);
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		hashmap.put(cell, value);
 	}
 	
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
+		String result = get(cell);
+		HashMap<String,Boolean> temp = new HashMap<String,Boolean>();
+		while( result.startsWith("="))
+		{
+			result = result.substring(1,result.length());
+			if( result.matches("[A-Z]+\\d") )
+			{
+				if( temp.containsKey(result) )
+					return "#Circular";
+				temp.put(result, true);
+				result = get(result);
+			}
+		}
+		if( result.startsWith("'"))
+		{
+			if( result.endsWith("'") )
+				result = result.substring(1, result.length()-1);
+			else
+				result = "#Error";
+		}
+		else
+		{
+			for( int i=0 ; i<result.length() ; i++ )
+			{
+				if( !( (result.charAt(i)>='0' && result.charAt(i)<='9') || result.charAt(i)=='-' ) ) 
+				{
+					result = "#Error";
+					break;
+				}
+			}
+		}
+		return result;
 	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -1,15 +1,88 @@
 import static org.junit.Assert.*;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class SpreadsheetTest {
 
+	Spreadsheet spt;
+	@Before
+	public void setup(){
+		spt = new Spreadsheet();
+	}
+	
+	
+	// test rule_1
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void Test_set_get_1_1() {
+		spt.set("A1", "1");
+		assertEquals("1",spt.get("A1"));
 	}
 
+	// test rule_2
+	@Test
+	public void Test_set_evaluate_minus1_minus1(){
+		spt.set("A1", "-1");
+		assertEquals("-1",spt.evaluate("A1"));
+	}
+	
+	// test rule_3
+	@Test
+	public void Test_set_evaluate_5A_error(){
+		spt.set("A1", "5A");
+		assertEquals("#Error",spt.evaluate("A1"));
+	}
+	
+	// test rule_4
+	@Test
+	public void Test_set_evaluate_astringWithQuotes_astring(){
+		spt.set("A1", "'a string'");
+		assertEquals("a string",spt.evaluate("A1"));
+	}
+	
+	// test rule_5
+	@Test
+	public void Test_set_evaluate_astringWithQuote_error(){
+		spt.set("A1", "'a string");
+		assertEquals("#Error",spt.evaluate("A1"));
+	}
+	
+	// test rule_6
+	@Test
+	public void Test_set_evaluate_equaltionmark_and_astringWithQuotes_astring(){
+		spt.set("A1", "='a string'");
+		assertEquals("a string",spt.evaluate("A1"));
+	}
+	
+	// test rule_7
+	@Test
+	public void Test_set_evaluate_equaltionmark_and_astringWithQuote_error(){
+		spt.set("A1", "='a string");
+		assertEquals("#Error",spt.evaluate("A1"));
+	}
+	
+	// test rule_8
+	@Test
+	public void Test_set_evaluate_equaltionmark_and_A5_5_5(){
+		spt.set("A1","=A5");
+		spt.set("A5","5");
+		assertEquals("5",spt.evaluate("A1"));
+	}
+	
+	// test rule_9
+	@Test
+	public void Test_set_evaluate_equaltionmark_and_A5_5A_error(){
+		spt.set("A1","=A5");
+		spt.set("A5", "5A");
+		assertEquals("#Error",spt.evaluate("A1"));
+	}
+	
+	// test rule_10
+	@Test
+	public void Test_set_evaluate_equaltionmark_and_A5_A5_circular(){
+		spt.set("A1","=A5");
+		spt.set("A5","=A1");
+		assertEquals("#Circular",spt.evaluate("A1"));
+	}
 }
