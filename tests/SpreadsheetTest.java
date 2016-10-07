32be444a3cd6144cad5c4c94f78c17cbe1e98c1d
somename <someemail>
2016-10-07 14:29:23 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -5,21 +5,26 @@ import org.junit.Test;
 
 
 public class SpreadsheetTest {
-
-	Spreadsheet ss = new Spreadsheet();
 	
 	@Test
 	public void spreadsheet_notnull() {
+		Spreadsheet ss = new Spreadsheet();
 		assertNotNull(ss);
 	}
 
 	@Test
 	public void spreadsheet_set_a1_1() {
+		Spreadsheet ss = new Spreadsheet();
 		ss.set("A1", "1");
-		
 		String result = ss.get("A1");
-		
 		assertEquals("1", result);
+	}
+	
+	@Test
+	public void spreadsheet_get_error() {
+		Spreadsheet ss = new Spreadsheet();
+		ss.set("A1", "2");
+		
 	}
 	
 	
