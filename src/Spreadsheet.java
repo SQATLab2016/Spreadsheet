eef728a527c5c61630b8ba495cfc8f89b1a209e9
somename <someemail>
2016-10-07 14:58:12 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
 
 public class Spreadsheet {
 
+	 ArrayList<String> cells=new ArrayList<String>();
+	 
+	 
+	
 	public String get(String cell) {
-		// to be implemented
-		return null;
+		          
+		return  cells.get(cells.indexOf(cell)+1);
+				
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		cells.add(cell);
+		cells.add(value);
+
 	}
 	
 	public String evaluate(String cell) {
