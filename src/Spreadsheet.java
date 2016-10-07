69448ac29fb19f148228a3c882c1795033a075cd
somename <someemail>
2016-10-07 16:44:10 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -66,7 +66,7 @@ public class Spreadsheet {
 						   }
 					   }else if(cells.contains(cells.get(cells.lastIndexOf(cell)+1).substring(1))){ // 8 =A1
 						   
-						   return cells.get(cells.lastIndexOf(cells.get(cells.lastIndexOf(cell)+1).substring(1))+1);
+						   return evaluate(cells.get(cells.lastIndexOf(cell)+1).substring(1));
 					   }
 				}else{
 					return cells.get(cells.lastIndexOf(cell)+1);
