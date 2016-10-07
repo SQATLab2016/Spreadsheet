76b261c49a3811568f3174927b9f9afcf1765256
somename <someemail>
2016-10-07 14:23:16 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -2,16 +2,22 @@ import java.util.*;
 
 public class Spreadsheet {
 
+	// dictionary of cells and values.
 	Map <String, String> map = new HashMap<String, String>();
 	
 	
 	public String get(String cell) {
-		// to be implemented
+		
+		
+		
 		return null;
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		
+		if(map.containsKey(cell))
+		
+		
 	}
 	
 	public String evaluate(String cell) {
