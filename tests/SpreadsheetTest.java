bf47de78b4764a2bccc40fbc0ff8e6b1ea593b6a
somename <someemail>
2016-10-12 15:06:11 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -70,6 +70,15 @@ public class SpreadsheetTest {
 		assertEquals(sheet.evaluate("C3"), "4");		
 		
 	}
+	
+	@Test
+	public void testSpreadsheetequalSignWithError_Int_setC3_EqualsD3_evaluateC3_return_Error(){
+		sheet.set("C3", "=D3");
+		
+		assertEquals(sheet.evaluate("C3"), "#ERROR");		
+		
+	}
+
 
 
 }
