a3f77a5260083cd691ac027e8e2508c6c06bc033
somename <someemail>
2016-10-12 16:32:47 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -9,7 +9,7 @@ public class SpreadsheetTest {
 	@Test
 	public void testGetCell() {
 		Spreadsheet sh = new Spreadsheet(5,5);
-		System.out.print(sh.get("D5"));
+		//System.out.print(sh.get("D5"));
 		assertEquals(sh.get("D5"),"7");
 		
 	}
@@ -18,7 +18,7 @@ public class SpreadsheetTest {
 	public void testSetCell() {
 		Spreadsheet sh = new Spreadsheet(5,5);
 		sh.set("A7", "6");
-		System.out.print(sh.get("A7"));
+		//System.out.print(sh.get("A7"));
 		assertEquals(sh.get("A7"),null);
 		
 	}
