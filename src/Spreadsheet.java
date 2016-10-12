d6dad3cd42f46bf72e19eea7b07a49842dbd271d
somename <someemail>
2016-10-12 14:43:34 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -71,7 +71,7 @@ public class Spreadsheet {
 	public String evaluate(String cell) {
 		String value = (String) mValues.get(cell);
 		
-		if (!(value.charAt(1) < '0' || value.charAt(1) > '9')) {
+		if (!(value.charAt(1) < '0' || value.charAt(1) > '9') && !value.equals("#Error")) {
 			if (mValues.containsKey(value)) {
 				return (String) mValues.get(value);
 				
