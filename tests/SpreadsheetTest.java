37628214658229d2281922cf6ca1bbf3eaf34340
somename <someemail>
2016-10-12 14:33:52 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -3,11 +3,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SpreadsheetTest {
+	
+	Spreadsheet sheet = new Spreadsheet();
 
 	@Test
 	public void testSpreadsheet_setA1_1_getA1_return1() {
-
-		Spreadsheet sheet = new Spreadsheet();
 		sheet.set("A1", "1");
 
 		assertEquals(sheet.get("A1"), "1");
@@ -16,14 +16,13 @@ public class SpreadsheetTest {
 	
 	@Test
 	public void testSpreadsheet_setB2_neg3_getB2_returnneg3() {
-
-		Spreadsheet sheet = new Spreadsheet();
 		sheet.set("B2", "-3");
 
 		assertEquals(sheet.get("B2"), "-3");
 
 	}
 
+	
 
 
 }
