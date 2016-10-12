58d8af048d71ac835e4238d51942b839fa1b60f9
somename <someemail>
2016-10-12 15:14:42 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -22,5 +22,13 @@ public class SpreadsheetTest {
 		
 	}
 	
+	@Test public void spreadsheetEvaluateA2_D4_returnsError() {
+		Spreadsheet spready = new Spreadsheet();
+		
+		spready.set("A2", "-5");
+		assertEquals("-5", spready.evaluate("A2"));	
+		
+	}
+	
 }
 
