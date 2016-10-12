31b64493016f3db100aeb359dd9b9275ee824465
somename <someemail>
2016-10-12 14:44:24 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -2,25 +2,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Spreadsheet {
-	
+
 	Map<String, String> sheet = new HashMap<>();
 
 	public String get(String cell) {
 		return sheet.get(cell);
 	}
-	
+
 	public void set(String cell, String value) {
 		sheet.put(cell, value);
 	}
-	
+
 	public String evaluate(String cell) {
-		try{
-		int intvalue= Integer.parseInt(sheet.get(cell));
-		
-		return sheet.get(cell);
-		}catch(NumberFormatException ex){
+		try {
+			Integer.parseInt(sheet.get(cell));
+			return sheet.get(cell);
+		} catch (NumberFormatException ex) {
 			return "#ERROR";
 		}
 	}
-	
+
 }
