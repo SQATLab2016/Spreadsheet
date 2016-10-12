c12de5ab301805cd88084484375073a36e281e5f
somename <someemail>
2016-10-12 16:22:10 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -53,10 +53,12 @@ public class SpreadsheetTest {
 		spreadsheet.set("A1", "'str with ' in the middle'");
 		
 		try {
-			spreadsheet.evaluate(cell);
+			spreadsheet.evaluate("A1");
 		} catch (SpreadsheetException e) {
-			System.out.println("Caught too many str marks exmsg: " + e.getMessage());
-			assertEquals("Caught different exception than expected", e.getMessage(), STR_MARK_IN_THE_MIDDLE_ERROR_MSG);
+			assertEquals("Caught different exception than expected",
+					e.getMessage(), spreadsheet.STR_MARK_IN_THE_MIDDLE_ERROR_MSG);
 		}
+		
+		fail("String mark not found in the middle of a string");
 	}
 }
