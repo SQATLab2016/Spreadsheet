1cf648964d41a8b499a832eb52986883228892b5
somename <someemail>
2016-10-12 15:42:08 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,8 +1,21 @@
 
 public class Spreadsheet {
-
+	Cell[][] cells;
+	
+	Spreadsheet(int row,int column) {
+		cells = new Cell[row][column];
+		for(int i=0; i<row;++i) {
+			char col = 'A';
+			for(int j=0; j<column;++j, ++col) {
+				
+				cell[i][j] = new Cell(Character.toString(col)+Integer.toString(i+1));
+			}
+		}
+	}
 	public String get(String cell) {
-		// to be implemented
+		
+		table.get(cell);
+		
 		return null;
 	}
 	
