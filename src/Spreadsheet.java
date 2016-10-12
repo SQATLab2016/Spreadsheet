deb477bbf84cec0d7a7f52eb3f002fdc85dbf189
somename <someemail>
2016-10-12 15:33:18 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -109,12 +109,12 @@ public class Spreadsheet {
 				
 				String subEquation = equation.substring(openingParenthesis + 1, closingParenthesis);
 				String calculatedSubEquation = calculateEquation(subEquation);
-				return calculatedSubEquation;
 				
-//				if (calculatedSubEquation.equals("#Error")) {
-//					return "#Error";
-//				}
-//				
+				if (calculatedSubEquation.equals("#Error")) {
+					return "#Error";
+				}
+
+				return calculatedSubEquation;
 //				equation = equation.replace(openingParenthesis, closingParenthesis, calculatedSubEquation);
 			}
 			
