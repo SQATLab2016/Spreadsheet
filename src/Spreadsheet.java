fbcf46ce6664145500e1083a2c11259bb5497b47
somename <someemail>
2016-10-12 14:32:50 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -3,20 +3,24 @@ import java.util.Map;
 
 public class Spreadsheet {
 
-	Map<String,String> sheet = new HashMap<String,String>();
-	
+	Map<String, String> sheet = new HashMap<String, String>();
+
 	public String get(String cell) {
 		return sheet.get(cell);
 	}
-	
+
 	public void set(String cell, String value) {
 		sheet.put(cell, value);
 	}
-	
+
 	public int evaluate(String cell) {
-		int result = 0;
-		result = Integer.parseInt(sheet.get(cell));
-		return result;
+		try {
+			int result = 0;
+			result = Integer.parseInt(sheet.get(cell));
+			return result;
+		} catch (Exception e) {
+			throw new SpreadSheetException();
+		}
 	}
-	
+
 }
