59e021a6eb10a76ccb68146e48df83158b0203f2
somename <someemail>
2016-10-12 16:05:46 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -3,10 +3,12 @@ public class Spreadsheet {
 	Cell[][] cells;
 	int row;
 	int column;
+	
 	Spreadsheet(int row,int column) {
 		this.row=row;
 		this.column=column;
 		cells = new Cell[row][column];
+		
 		for(int i=0; i<row;++i) {
 			char col = 'A';
 			for(int j=0; j<column;++j, ++col) {
@@ -14,14 +16,16 @@ public class Spreadsheet {
 				cells[i][j] = new Cell(Character.toString(col)+Integer.toString(i+1));
 			}
 		}
+		
 	}
+	
 	public String get(String cell) {
 		
 		for(int i=0; i<row;++i) {
-		
 			for(int j=0; j<column;++j) {
 				
-				if(cells[i][j].location.equals(cell)) return cells[i][j].value;
+				if(cells[i][j].location.equals(cell)) 
+					return cells[i][j].value;
 				
 			}
 		}
@@ -30,7 +34,31 @@ public class Spreadsheet {
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		
+		for(int i=0; i<row;++i) {
+			for(int j=0; j<column;++j) {
+				
+				if(cells[i][j].location.equals(cell)) { 
+					cells[i][j].value = value;
+					return;
+				}	
+				
+			}
+		}
+		
+	}
+	Index getCellIndex(String cell){
+		Index index = null;
+		for(int i=0; i<row;++i) {
+			for(int j=0; j<column;++j) {
+				
+				if(cells[i][j].location.equals(cell)) {
+					index = new Index(i,j);
+					return index;
+				}
+				
+			}
+		}
 	}
 	
 	public String evaluate(String cell) {
