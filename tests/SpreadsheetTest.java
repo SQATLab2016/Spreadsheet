d2f98a6bcf3ddd2bd7ad1afed9c063d96dff3f5d
somename <someemail>
2016-10-12 15:05:59 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -118,9 +118,11 @@ public class SpreadsheetTest {
 		String result = sheet.evaluate("A1");
 		assertEquals("#Error", result);
 	}
+	
 	@Test
 	public void testAssignment_Reference_Circular_Error() {
-		sheet.set("A2", "=A5");
+		sheet.set("A1", "=A2");
+		sheet.set("A2", "=A1");
 		String result = sheet.evaluate("A2");
 		assertEquals("#Circular", result);
 	}
