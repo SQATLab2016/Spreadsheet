aba2cf2e969168617b0469fc9139e47a0a53dd94
somename <someemail>
2016-10-12 14:43:28 +0300

    besouro automatic message

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,36 @@
 import static java.lang.*;
 
 public class Cell {
+	private String cellName;
+	private String cellValue;
 	
 	Cell(String cell, String value){
-		this.setName(cell);
+		this.setCellName(cell);
+		this.setCellValue(value);
 		
 	}
+	
+	
+	
+	
+	
+	
+	public String getCellName() {
+		return cellName;
+	}
+
+	public void setCellName(String cellName) {
+		this.cellName = cellName;
+	}
+
+	public String getCellValue() {
+		return cellValue;
+	}
+
+	public void setCellValue(String cellValue) {
+		this.cellValue = cellValue;
+	}
+
+	
+	
 }
