1b9e5dd5240ba68be9217d6e5af40e2f9a7a5e21
somename <someemail>
2016-10-12 14:30:27 +0300

    besouro automatic message

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -24,4 +24,10 @@ public class SpreadsheetTest {
 		oneSheet.set("A1", "-1");
 		assertEquals(-1, oneSheet.evaluate("A1"));
 	}
+	
+	@Test
+	public void testSpreadSheet_evaluateCellWrongFormatedInteger_Exception() {
+		Spreadsheet oneSheet = new Spreadsheet();
+		oneSheet.set("A1", "5A");
+	}
 }
