4a8e4979bc24596f3fae121af0f272324375c9e3
somename <someemail>
2016-10-12 16:00:39 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -29,13 +29,38 @@ public class Spreadsheet {
 				value.charAt(0) == '-' || (value.charAt(0) >= '0' && value.charAt(0) <= '9'));
 	}
 	
+	private String evaluateInteger(String value) {
+		boolean isInteger = true;
+		for (int i = 0; i < value.length(); i++) {
+			char c = value.charAt(i);
+			if (i == 0 && c == '-')
+				continue;
+			
+			if (c < '0' || c > '9') {
+				isInteger = false;
+				break;
+			}
+		}
+		
+		if (isInteger) {
+			return value;
+		} else {
+			return ERROR;
+		}
+	}
+	
 	private boolean isString(String value) {
 		return value.length() > 0 && (value.charAt(0) == '\'' ||
 				value.charAt(value.length() - 1) == '\'');
 	}
 	
-	private String evaluateString(String value) {value.charAt(0) == '\'' &&
-			value.charAt(value.length() - 1) == '\'')
+	private String evaluateString(String value) {
+		if (value.charAt(0) == '\'' &&
+			value.charAt(value.length() - 1) == '\'') {
+			return value.substring(1, value.length() - 1);
+		} else {
+			return ERROR;
+		}
 	}
 	
 	private boolean isCircularRecursive(String value) {
@@ -74,29 +99,8 @@ public class Spreadsheet {
 				value = value.substring(1, value.length());
 				
 				if (isString(value)) {
-					if (value.charAt(0) == '\'' &&
-						value.charAt(value.length() - 1) == '\'') {
-						value = value.substring(1, value.length() - 1);
-					} else {
-						value = ERROR;
-					}
 					
 				} else if (isInteger(value)) {
-					boolean isInteger = true;
-					for (int i = 0; i < value.length(); i++) {
-						char c = value.charAt(i);
-						if (i == 0 && c == '-')
-							continue;
-						
-						if (c < '0' || c > '9') {
-							isInteger = false;
-							break;
-						}
-					}
-					
-					if (!isInteger) {
-						value = ERROR;
-					}
 					
 				} else if (isReference(value)) {
 					if (mValues.containsKey(value)) {
