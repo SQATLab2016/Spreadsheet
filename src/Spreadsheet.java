4482c425c79de5964824ad006687ee56b2b655b0
somename <someemail>
2016-10-12 15:44:44 +0300

    besouro automatic message

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -14,8 +14,12 @@ public class Spreadsheet {
 	public void set(String cell, String value) throws SpreadsheetException{
 		// to be implemented
 		//Test if contains only numbers 0-9
-		if(value.matches("[0-9]+") &&(!(value.contains("[a-zA-Z]+")))) throw new SpreadsheetException("sjadasdajsd");
+		//if(value.matches("[0-9]+") &&(!(value.contains("[a-zA-Z]+")))) throw new SpreadsheetException("sjadasdajsd");
 		
+	    for (int i = 0; i < value.length(); i++) {
+	        if (!Character.isDigit(value.charAt(i)))
+	        	throw new SpreadsheetException("sjadasdajsd");
+	      }
 		//Put the cell and value to excell hashmap
 
 		excell.put(cell, value);	
