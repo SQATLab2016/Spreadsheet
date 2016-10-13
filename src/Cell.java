61bc2bea6e11a238282f3a2b9ccbdb2ef25a511c
skorkko <skorkko@STUPID-PC705.stnet.local>
2016-10-13 16:41:43 +0300

    req 8

--- src/Cell.java
+++ src/Cell.java
@@ -3,18 +3,26 @@ public class Cell {
 	
 	private String cellName;
 	private String cellContent;
-	private int cellInt;
 	private boolean isNumber;
+	private boolean isFormula;
 	
 	public Cell(String name, String content) {
 		this.cellName = name;
 		this.cellContent = content;
 		this.isNumber = false;
+		this.isFormula = false;
 	}
 	
 	public String getCellContent() {
-		if(isNumber) {
-			return Integer.toString(cellInt);
+		if(isFormula || isNumber) {
+			return this.cellContent;
+		}
+		if(cellContent.charAt(0) == '\'' && cellContent.charAt(cellContent.length()-1) == '\'') {
+			StringBuilder build = new StringBuilder(this.cellContent);
+				build.deleteCharAt(0);
+				build.deleteCharAt(cellContent.length()-2);
+			String result = build.toString();
+			return result;
 		}
 		return this.cellContent;
 	}
@@ -31,8 +39,30 @@ public class Cell {
 			this.isNumber = false;
 			return;
 		}
+		this.cellContent = content;
+		this.isNumber = true;
+	}
+	
+	public void setFormula(boolean isFormula) {
+		this.isFormula = isFormula;
+	}
+	
+	public boolean isFormula() {
+		return this.isFormula;
+	}
+	
+	/*
+	public void setCellContent(String content) {
+		try {
+			int i = Integer.parseInt(content);
+		} catch(NumberFormatException nfe) {
+			this.cellContent = content;
+			this.isNumber = false;
+			return;
+		}
 		this.cellContent = "";
 		this.isNumber = true;
 		this.cellInt = Integer.parseInt(content);
 	}
+	*/
 }
--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -14,19 +14,16 @@ public class Spreadsheet {
 		cells.add(new Cell("A5", ""));
 	}
 	
-	public String get(String cell) {
-		for(Cell c : cells) {
-			if(c.getCellName() == cell) {
-				return c.getCellContent();
-			}
-		}
-		throw new IllegalArgumentException("Cell doesn't exist");
-	}
-	
 	public void set(String cell, String value) {
+		boolean isFormula = false;
+		if(value.charAt(0) == '=') {
+			 isFormula = true;
+		}
+		value = checkErrors(value);
 		for(Cell c : cells) {
 			if(c.getCellName() == cell) {
 				c.setCellContent(value);
+				c.setFormula(isFormula);				
 				return;
 			} 
 		}
@@ -34,8 +31,48 @@ public class Spreadsheet {
 	}
 	
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
+		for(Cell c : cells) {
+			//System.out.println("Cell: " + c.getCellName() + ", Content: " + c.getCellContent() + ", isFormula: " + c.isFormula());
+			if(c.getCellName() == cell) {
+				System.out.println(c.getCellName());
+				if(c.isFormula()) {		
+					evaluate(c.getCellContent());
+				} 
+				
+				return c.getCellContent();
+			}
+		}
+		throw new IllegalArgumentException("Cell doesn't exist");
+	}
+	
+	private String checkErrors(String value) {
+		System.out.println(value);
+		if(value.charAt(0) == '=') {
+			value = value.substring(1, value.length());
+			System.out.println(value);
+		}
+		if(value.charAt(0) == '\'' && value.charAt(value.length()-1) == '\'') {
+			return value;
+		}
+		if(isCellReference(value)) {
+			return value;
+		}
+		try {
+			int i = Integer.parseInt(value);
+		} catch(NumberFormatException nfe) {
+			value = "#Error";
+			return value;
+		}
+		return value;
+	}
+	
+	private boolean isCellReference(String value) {
+		for(Cell c : cells) {
+			if(c.getCellName().equals(value)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
