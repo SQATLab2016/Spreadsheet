6b31a76ad35213140c5e630def47ee414d797cee
pasilack <pasilack@STUPID-PC704.stnet.local>
2016-10-13 17:02:45 +0300

    11 finished

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Spreadsheet {
 	private ArrayList<Cell> cells = new ArrayList<Cell>();
+	private ArrayList<String> circularReferences = new ArrayList<String>();
 	
 	
 	public String get(String cell) {
@@ -21,29 +22,131 @@ public class Spreadsheet {
 		char c = value.charAt(0);
 		
 		if(Character.isDigit(c) || c =='-') {
-			int counter = 0;
-			for(char ch: value.toCharArray()){
-				if(ch == '-' && counter == 0) {
-					
-				}
-					
-				else if(!Character.isDigit(ch))
-					return "#Error";
-				counter++;
+			return dealWithNumbers(value);
+		}
+		
+		else if(c== '=') {
+			StringBuilder sb = new StringBuilder(value);
+			sb.deleteCharAt(0);
+			if(isAValidString(sb.toString(), sb.charAt(0))) {
+				return dealWithStrings(sb.toString());
 			}
-			return value;
+			
+			else if(Character.isDigit(sb.charAt(0)) || sb.charAt(0) =='-') {
+				return dealWithNumbers(sb.toString());
+			}
+			
+			//definition of cell #ID? Uppercase A-Z, number
+			else if(Character.isUpperCase(sb.charAt(0)) && Character.isDigit(sb.charAt(1)) && 2 == sb.length()) {
+				String cellName = sb.toString();
+				if(circularReferences.contains(cellName)){
+					return "#Circular";
+				}
+				else {
+					circularReferences.add(cellName);
+					return evaluate(cellName);
+				}
+			}
+			
+			else 
+				return "#Error";
 		}
 		
 		else {
-			if(c == '\'' && value.substring(value.length() - 1).equals("'")) {
-				StringBuilder sb = new StringBuilder(value);
-				sb.deleteCharAt(0);
-				sb.deleteCharAt(sb.length()-1);
-				return sb.toString();
+			if(isAValidString(value, c)) {
+				return dealWithStrings(value);
 			}
 			else
 				return "#Error";
 		}
+	}
+	
+	
+	
+	
+	public String dealWithStrings(String input) {
+		StringBuilder sb = new StringBuilder(input);
+		sb.deleteCharAt(0);
+		sb.deleteCharAt(sb.length()-1);
+		return sb.toString();
+	}
+	
+	
+	
+	
+	public String dealWithNumbers(String input) {
+		String value = input;
+		int counter = 0;
+		for(char ch: value.toCharArray()){
+			if(ch == '-' && counter == 0) {
+				
+			}
+			else if(Character.isDigit(ch) || ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%') {
+				
+			}
+			else
+				return "#Error";
+			counter++;
+		}
+
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		ArrayList<Character> operations = new ArrayList<Character>();
+		
+		int result = 0;
+		for (int i=0; i < value.length(); i++) {
+		    char c = value.charAt(i);
+		    if (Character.isDigit(c)) {
+		    	result = result*10 + Character.getNumericValue(c);
+		    	if(i == value.length()-1)
+		    		values.add(result);
+		    }
+		    else {
+		    	values.add(result);
+		    	operations.add(c);
+		    	result = 0;
+		    }
+		    
+		}
+
+		int calculated = 0;
+		int operationCounter = -1;
+		for(int v : values) {
+			if(operationCounter == -1) {
+				calculated = v;
+			}
+			else {
+				char c = operations.get(operationCounter);
+				switch(c) {
+				case '+' :
+					calculated += v;
+					break;
+				case '-' :
+					calculated -= v;
+					break;
+				case '*' :
+					calculated = calculated * v;
+					break;
+				case '/' :
+					calculated = calculated / v;
+					break;
+				case '%' :
+					calculated = calculated % v;
+					break;
+				}
+			}
+			operationCounter++;
+		}
+		value = "" + calculated;
+		return value;
+	}
+	
+	
+	
+	public boolean isAValidString(String testable, char first) {
+		if(first == '\'' && testable.substring(testable.length() - 1).equals("'")) {
+			return true;
+		}
+		return false;
 	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -16,40 +16,140 @@ public class SpreadsheetTest {
 	public void testCreateCellA1() {
 		sheet.set("A1", "1");
 		String s = sheet.get("A1");
-		assertEquals("1", s);
-				
+		assertEquals("1", s);		
 	}
 	
 	@Test
 	public void testCreateCellA1_minus1() {
 		sheet.set("A1", "-1");
 		String s = sheet.evaluate("A1");
-		assertEquals("-1", s);
-				
+		assertEquals("-1", s);		
 	}
 	
 	@Test
 	public void testCreateCellA1_5A() {
 		sheet.set("A1", "5A");
 		String s = sheet.evaluate("A1");
-		assertEquals("#Error", s);
-				
+		assertEquals("#Error", s);		
 	}
 	
 	@Test
 	public void testCreateCellA1astring() {
 		sheet.set("A1", "'a string'");
 		String s = sheet.evaluate("A1");
-		assertEquals("a string", s);
-				
+		assertEquals("a string", s);		
 	}
 	
 	@Test
-	public void testCreateCellA1astringWithBadFormat() {
+	public void testCreateCellA1astringWithNoEndQuote() {
 		sheet.set("A1", "'a string");
 		String s = sheet.evaluate("A1");
+		assertEquals("#Error", s);	
+	}
+	
+	@Test
+	public void testCreateCellA1astringWithNoStartQuote() {
+		sheet.set("A1", "a string'");
+		String s = sheet.evaluate("A1");
+		assertEquals("#Error", s);	
+	}
+	
+	@Test
+	public void testCreateCellA1WithSimpleEquationResultingInString() {
+		sheet.set("A1", "='a string'");
+		String s = sheet.evaluate("A1");
+		assertEquals("a string", s);		
+	}
+	
+	@Test
+	public void testCreateCellA1WithSimpleEquationStringWithError() {
+		sheet.set("A1", "=a string'");
+		String s = sheet.evaluate("A1");
+		assertEquals("#Error", s);		
+	}
+	
+	@Test
+	public void testCreateCellA1WithCellReferenceA5() {
+		sheet.set("A5", "3");
+		sheet.set("A1", "=A5");
+		String s = sheet.evaluate("A1");
+		assertEquals("3", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithCellReferenceA5WithErrorInIt() {
+		sheet.set("A5", "5A");
+		sheet.set("A1", "=A5");
+		String s = sheet.evaluate("A1");
 		assertEquals("#Error", s);
-				
+	}
+	
+	@Test
+	public void testCreateCellA1WithCircularCellReference() {
+		sheet.set("A5", "=A1");
+		sheet.set("A1", "=A5");
+		String s = sheet.evaluate("A1");
+		assertEquals("#Circular", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithCircularCellReferences() {
+		sheet.set("A5", "=A4");
+		sheet.set("A4", "=A3");
+		sheet.set("A3", "=A2");
+		sheet.set("A2", "=A1");
+		sheet.set("A1", "=A5");
+		String s = sheet.evaluate("A1");
+		assertEquals("#Circular", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithIntegerOperation5Plus1() {
+		sheet.set("A1", "=5+1");
+		String s = sheet.evaluate("A1");
+		assertEquals("6", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithIntegerOperation5Minus1() {
+		sheet.set("A1", "=5-1");
+		String s = sheet.evaluate("A1");
+		assertEquals("4", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithIntegerOperation5Divided1() {
+		sheet.set("A1", "=5/1");
+		String s = sheet.evaluate("A1");
+		assertEquals("5", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithIntegerOperation5Times2() {
+		sheet.set("A1", "=5*2");
+		String s = sheet.evaluate("A1");
+		assertEquals("10", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithIntegerOperation5Modus1() {
+		sheet.set("A1", "=5%1");
+		String s = sheet.evaluate("A1");
+		assertEquals("0", s);
+	}
+	
+	@Test
+	public void testCreateCellA1WithIntegerOperation555Plus1() {
+		sheet.set("A1", "=555+1");
+		String s = sheet.evaluate("A1");
+		assertEquals("556", s);
+	}
+	
+	@Test
+	public void testCreateCellA1Equals1() {
+		sheet.set("A1", "=1");
+		String s = sheet.evaluate("A1");
+		assertEquals("1", s);
 	}
 
 }
