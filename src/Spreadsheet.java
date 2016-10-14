1d333ceed7cc7ec8cfca70451334177fd8387e1d
jkorpeli <jkorpeli@STUPID-PC728.stnet.local>
2016-10-14 16:44:18 +0300

    done until 5.

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,18 +1,35 @@
+import java.util.HashMap;
 
 public class Spreadsheet {
-
+	
+	private HashMap<String, String> hm = new HashMap<String, String>();
+	
 	public String get(String cell) {
-		// to be implemented
-		return null;
+		return hm.get(cell);
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		hm.put(cell, value);
 	}
 	
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
+		
+		String value = hm.get(cell);
+		String first = value.substring(0,1);
+		String last = value.substring(value.length() - 1);
+
+		if(isNumeric(value)){
+			return value;
+		}else if(first.equals("'") && last.equals("'")){
+			return value.substring(1, value.length()-1);
+		}else{
+			return "#Error";
+		}	
+	}
+	
+	public static boolean isNumeric(String str)
+	{
+	  return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
 	}
 	
 }
