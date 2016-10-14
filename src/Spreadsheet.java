e88c710967523a4af707fd0cd3a6cc49bcd1b477
julaakko <julaakko@STUPID-PC708.stnet.local>
2016-10-14 16:52:30 +0300

    Assigment, requirements 8-9

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 public class Spreadsheet {
 
 	private HashMap<String, String> sheetmap = new HashMap<String, String>();
+	private static final String ERROR = "#Error";
 	
 	public String get(String cell) {	
 		return sheetmap.get(cell);	
@@ -14,8 +15,52 @@ public class Spreadsheet {
 	
 	public String evaluate(String cell) {
 		
-		return sheetmap.get(cell);
+		String content = get(cell);		
+		if(isCellReference(content))
+			content = get(content);		
+					
+		return validateCell(content);
 	}
+	
+	private boolean isCellReference(String contents) {				
+			return sheetmap.containsKey(contents);
+	}
+	
+	private boolean checkFormula(String contents) {		
+		if(contents.startsWith("="))
+			return true;
+		return false;
+	}
+	
+	private boolean checkString(String contents) {
+		if(contents.startsWith("'") && contents.endsWith("'"))
+			return true;	
+		else
+			return false;
+	}
+	
+	private boolean checkInteger(String contents) {
+		try  {
+			Integer.parseInt(contents);
+			
+			} catch (NumberFormatException exp) {
+				return false;
+			}		
+		return true;
+	}
+	
+	private String validateCell(String contents) {	
 
+		if(checkFormula(contents))
+			return validateCell(contents.substring(1,contents.length()));
+		
+		if(checkString(contents))
+			return contents.substring(1, contents.length()-1);
+
+		if(checkInteger(contents))
+			return contents;
+		
+		return ERROR;
+	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -6,6 +6,15 @@ import org.junit.Test;
 
 public class SpreadsheetTest {
 
+	private static final String ERROR = "#Error";
+	private static final String VALID_INPUT_A_STRING_CATCHES = "'A string catches'";
+	private static final String VALID_INPUT_FORMULA_A_STRING_CATCHES = "='A string catches'";
+	private static final String INVALID_INPUT_FORMULA_A_STRING_CATCHES = "A string catches'";	
+	private static final String VALID_EVALUATE_A_STRING_CATCHES = "A string catches";
+	private static final String STRING_FAIL_BOAT_START_WRONG = "Fail boat'";
+	private static final String STRING_FAIL_BOAT_END_WRONG = "'Fail boat";
+	private static final String STRING_FAIL_BOAT_NO_QUOTES = "Fail boat";
+	
 	private Spreadsheet test_sheet = new Spreadsheet();
 	
 	@Test	
@@ -17,22 +26,115 @@ public class SpreadsheetTest {
 	@Test
 	public void test_evaluateCorrectlyFormattedCellPositiveInteger() {
 		test_sheet.set("A1", "1");
-		String result = test_sheet.evaluate("A1");
-		assertEquals("1", result);
-		
+		assertEquals("1", evaluateCell("A1"));		
 	}
 	
 	@Test
 	public void test_evaluateCorrectlyFormattedCellNegativeInteger() {
-		test_sheet.set("A1", "-1");
-		String result = test_sheet.evaluate("A1");
-		assertEquals("-1", result);		
+		test_sheet.set("A1", "-1");		
+		assertEquals("-1", evaluateCell("A1"));		
 	}
 	
 	@Test
 	public void test_evaluateInvalidFormattedCellInteger() {
-		test_sheet.set("A1", "A3");
-		String result = test_sheet.evaluate("A1");
-		assertEquals("#Error", result);		
+		test_sheet.set("A1", "A3");		
+		assertEquals(ERROR, evaluateCell("A1"));		
 	}
+	
+	@Test 
+	public void test_evaluateCatchesDecimalsInCellSeparatorDot() {
+		test_sheet.set("A1", "4.2");
+		assertEquals(ERROR, evaluateCell("A1"));
+	}
+	
+	@Test 
+	public void test_evaluateCatchesDecimalsInCellSeparatorComma() {
+		test_sheet.set("A1", "4,2");		
+		assertEquals(ERROR, evaluateCell("A1"));
+	}
+	
+	@Test 
+	public void test_canSetGetStringsCell() {
+		test_sheet.set("A7", VALID_INPUT_A_STRING_CATCHES);
+		String result = test_sheet.get("A7");
+		assertEquals(VALID_INPUT_A_STRING_CATCHES, result);
+	}
+	
+	@Test
+	public void test_evaluateValidQuotedStringReturnsUnquotedString() {
+		test_sheet.set("A1", VALID_INPUT_A_STRING_CATCHES);
+		assertEquals(VALID_EVALUATE_A_STRING_CATCHES, evaluateCell("A1"));
+	}
+	
+	@Test
+	public void test_evaluateInvalidNonQuotedString() {
+		test_sheet.set("A1", STRING_FAIL_BOAT_NO_QUOTES);
+		assertEquals(ERROR, evaluateCell("A1"));		
+	}
+	
+	@Test
+	public void test_evaluateInvalidQuotedStringStart() {
+		test_sheet.set("A1", STRING_FAIL_BOAT_START_WRONG);
+		assertEquals(ERROR, evaluateCell("A1"));		
+	}
+	
+	@Test
+	public void test_evaluateInvalidEndQuotedString() {
+		test_sheet.set("A1", STRING_FAIL_BOAT_END_WRONG);
+		assertEquals(ERROR, evaluateCell("A1"));		
+	}
+	
+	@Test
+	public void test_evaluateSimpleFormulaIntegerInput() {
+		test_sheet.set("A1", "=1");		
+		assertEquals("1", evaluateCell("A1"));
+	}
+	
+	@Test
+	public void test_evaluateSimpleFormulaSignedIntegerInput() {
+		test_sheet.set("A1", "=-11");		
+		assertEquals("-11", evaluateCell("A1"));
+	}	
+	
+	@Test
+	public void test_evaluateSimpleFormulaInvalidIntegerInput() {
+		test_sheet.set("A1", "=A51");		
+		assertEquals(ERROR, evaluateCell("A1"));
+	}
+	
+	@Test
+	public void test_evaluateSimpleFormulaStringInput() {
+		test_sheet.set("A1", VALID_INPUT_FORMULA_A_STRING_CATCHES);
+		assertEquals(VALID_EVALUATE_A_STRING_CATCHES, evaluateCell("A1"));
+	}
+	
+	@Test
+	public void test_evaluateSimpleFormulateInvalidStringInput() {
+		test_sheet.set("A1", INVALID_INPUT_FORMULA_A_STRING_CATCHES);
+		assertEquals(ERROR, evaluateCell("A1"));
+	}
+	
+	@Test
+	public void test_evaluateFormulaReferencesReturnsLastCellContentSingleRerence() {
+		test_sheet.set("A1", "A5");
+		test_sheet.set("A5", "1");
+		assertEquals("1", evaluateCell("A1"));
+	}
+	
+	/* Run in to corner with this, trying to see if moving forward helps to bubble up better solution
+	 * ... and then i realized the re-factoring i did was wrong...
+	@Test
+	public void test_evaluateFormulaReferencesReturnsLastCellContent3ChainRerence() {
+		test_sheet.set("A1", "A5");
+		test_sheet.set("A5", "A3");
+		test_sheet.set("A3", "A7");
+		test_sheet.set("A7", "1");
+		assertEquals("1", evaluateCell("A1"));
+	}*/
+	
+	
+	private String evaluateCell(String cell) {
+		return test_sheet.evaluate(cell);
+	}
+	
 }
