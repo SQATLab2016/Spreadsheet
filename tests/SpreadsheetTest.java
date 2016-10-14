a7c0a8e03141a01127105aa3544e0450c9c8434b
alang <alang@STUPID-PC729.stnet.local>
2016-10-14 16:45:31 +0300

    Finished exercise - #5

--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,9 +7,41 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void testCreate() {
+		Spreadsheet sp = new Spreadsheet();
+		assertNotNull(sp);
 	}
-
+	
+	@Test
+	public void testGet() {
+		Spreadsheet sp = new Spreadsheet();
+		sp.set("A1", "5");
+		String cv = sp.get("A1");
+		assertEquals("5", cv);
+	}
+	
+	@Test
+	public void testEvaluate_integer() {
+		Spreadsheet sp = new Spreadsheet();
+		sp.set("A1", "1");
+		String cv = sp.evaluate("A1");
+		assertEquals("1", cv);
+	}
+	
+	@Test
+	public void testEvaluate_quoted_string() {
+		Spreadsheet sp = new Spreadsheet();
+		sp.set("A1", "’abc’");
+		String cv = sp.evaluate("A1");
+		assertEquals("abc", cv);
+	}
+	
+	@Test
+	public void testEvaluate_incorrect_value() {
+		Spreadsheet sp = new Spreadsheet();
+		sp.set("A1", "’abc1");
+		String cv = sp.evaluate("A1");
+		assertEquals("#Error", cv);
+	}
+	
 }
