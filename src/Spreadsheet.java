25f1ec9c36bea3d7ba96977b8b7eed107b0b165c
sahelttu <sahelttu@10.30.7.16>
2016-10-14 16:48:16 +0300

    Samuli Helttunen

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,18 +1,30 @@
+import java.util.ArrayList;
 
 public class Spreadsheet {
 
-	public String get(String cell) {
-		// to be implemented
-		return null;
-	}
+	private ArrayList<Cell> cells =  new ArrayList<Cell>();
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		Cell C = new Cell();
+		C.setId(cell);
+		C.setValue(value);
+		cells.add(C);
 	}
+	
+	
+	public String get(String cell) {
+		
+		cell.get(id);
+		return cell;
+	}
+	
+
 	
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
-	}
 	
+		if (cell.substring(0).contains("[1-9]+") && (cell.substring(0).matches("[1-9]+"))){
+			return cell;	
+		}		
+		return "#Error";
+	}	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -7,9 +7,24 @@ import org.junit.Test;
 public class SpreadsheetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_EvaluatePass() {
+		Spreadsheet evaluate = new Spreadsheet();
+		evaluate.set("A3", "5");
+		String result = evaluate.evaluate("A3");
+		assertEquals("Contains only numbers", "5", result);
+		
 		
 	}
 
-}
+
+
+	@Test
+	public void test_set(){
+		Spreadsheet set = new Spreadsheet();
+		set.set("A3", "5");
+		String result = set.get("A3");
+		assertEquals("5", result);
+	}
+
+	
+	}
