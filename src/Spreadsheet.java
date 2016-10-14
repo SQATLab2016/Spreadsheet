bcc7dccdc27a6d52af2eae6f58ebe875e4c20ca3
mikkoyli <mikkoyli@STUPID-PC722.stnet.local>
2016-10-14 16:44:54 +0300

    Finished 7. Simple formulas with errors

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Spreadsheet {
 	
@@ -13,9 +15,54 @@ public class Spreadsheet {
 		cells.put(cell, value);
 	}
 	
+	public void isReference(String value) {
+		;// TODO
+	}
+	
+	public void getReference(String value) {
+		;// TODO
+	}
+	
+	public void isFormula(String value) {
+		;// TODO
+	}
+	
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
+		String value = cells.get(cell);
+		String return_text = "#Error";
+		Boolean isEquals = false;
+		
+		String firstLetter = value.substring(0, 1);
+		
+		if (firstLetter.equals("="))
+		{
+			value = value.substring(1);
+			isEquals = true;
+			if (!value.matches("[A-Z]"))
+			{
+				;//System.out.println(value);
+			}
+		}
+		
+		try { 
+	        Integer.parseInt(value); 
+	        return value;
+	    } catch(NumberFormatException e) { 
+	    	;//
+	    } catch(NullPointerException e) {
+	    	;//
+	    }
+		
+		Pattern p = Pattern.compile("(?:^|\\s)'([^']*?)'(?:$|\\s)", Pattern.MULTILINE);
+		
+		Matcher m = p.matcher(value);
+
+		while(m.find()) {
+			return_text = m.group(1);
+        }
+		
+		return return_text;
+		
 	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -13,5 +13,56 @@ public class SpreadsheetTest {
 		ss.set("A1", "1");
 		assertEquals("1", ss.get("A1"));
 	}
+	
+	@Test
+	public void test_set_A1_minus_1() {
+		ss.set("A1", "-1");
+		assertEquals("-1", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_set_5A_returns_error() {
+		ss.set("A1", "5A");
+		assertEquals("#Error", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_set_a_string_evaluates_string() {
+		ss.set("A1", "'A string'");
+		assertEquals("A string", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_set_invalid_string_evaluates_as_error() {
+		ss.set("A1", "'A string");
+		assertEquals("#Error", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_set_equals_to_string_evaluates_as_string() {
+		ss.set("A1", "='A string'");
+		assertEquals("A string", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_set_equals_to_invalid_string_evaluates_as_error() {
+		ss.set("A1", "='A string");
+		assertEquals("#Error", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_reference_from_A5_returns_5() {
+		ss.set("A5", "5");
+		ss.set("A1", "=A5");
+		assertEquals("5", ss.evaluate("A1"));
+	}
+	
+	@Test
+	public void test_reference_from_invalid_A5_returns_error() {
+		ss.set("A5", "5A");
+		ss.set("A1", "=A5");
+		assertEquals("#Error", ss.evaluate("A1"));
+	}
+	
 
 }
