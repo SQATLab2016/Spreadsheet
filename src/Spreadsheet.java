0dd5d0fb0fcfa9ea224c04b2080649e0394edcc9
ihietani <ihietani@STUPID-PC730.stnet.local>
2016-10-14 16:35:06 +0300

    Finishing later

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -21,8 +21,62 @@ public class Spreadsheet {
 	
 	public String evaluate(String cell) {
 		// to be implemented
+		String ev = cells.get(cell);
+		if(checkEquation(ev)){
+			ev = ev.substring(1);
+			if(ev.charAt(0) != '\''){
+				if(isCircular(cell, cells.get(ev))){
+					return "#Circular";
+				}
+				ev = evaluate(ev);
+			}
+		}
+	    if(checkFormat(ev)){
+	    	if(ev.charAt(0) == '\''){
+	    		ev = ev.substring(1, ev.length() - 1);
+	    		return ev;
+	    	}
+	    	return ev;
+	    }
+	    return "#Error";
+	}
+	public boolean checkFormat(String cell){
+		for(int i = 0; i < cell.length(); i++){
+			if(cell.charAt(0) == '-'){
+				continue;
+			}
+			else if(cell.charAt(0) == '\'' && 
+					cell.charAt(cell.length() - 1) == '\''){
+				return true;
+			}
+			else if(cell.charAt(i) >= '0' && cell.charAt(i) <= '9'){
+				continue;
+			}
+			else{
+				return false;
+			}
+		}
+		return true;
+	}
+	public boolean checkEquation(String cell){
+		if(cell.charAt(0) == '='){
+			return true;
+		}
+		return false;
+	}
+	public boolean isCircular(String current, String next){
+		boolean circular = false;
+		if(checkEquation(next)){
+			next = next.substring(1);
+		}
+		if(current.equals(next)){
+			return true;
+		}
 		
-		return cells.get(cell);
+		
+		return circular;
+		
+		
 	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -44,6 +44,76 @@ public class SpreadsheetTest {
 		
 		assertEquals("Evaluation is not correct", "#Error", n.evaluate("A1"));
 	}
+	@Test
+	public void test_String() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A1", "'This is correct'");
+		
+		assertEquals("Evaluation is not correct", "This is correct", n.evaluate("A1"));
+	}
+	@Test
+	public void test_StringUnquated() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A1", "'This is incorrect");
+		
+		assertEquals("Evaluation is not correct", "#Error", n.evaluate("A1"));
+	}
+	@Test
+	public void test_SimpleFormulas() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A1", "='a string'");
+		
+		assertEquals("Evaluation is not correct", "a string", n.evaluate("A1"));
+	}
+	@Test
+	public void test_SimpleFormulasError() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A1", "='a string");
+		
+		assertEquals("Evaluation is not correct", "#Error", n.evaluate("A1"));
+	}
+	@Test
+	public void test_CellReference() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A2", "5");
+		n.set("A1", "=A2");
+		
+		assertEquals("Evaluation is not correct", "5", n.evaluate("A1"));
+	}
+	@Test
+	public void test_CellReferenceError() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A2", "k5");
+		n.set("A1", "=A2");
+		
+		assertEquals("Evaluation is not correct", "#Error", n.evaluate("A1"));
+	}
+	@Test
+	public void test_CircularReference() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A2", "=A1");
+		n.set("A1", "=A2");
+		
+		assertEquals("Evaluation is not correct", "#Circular", n.evaluate("A1"));
+	}
+	@Test
+	public void test_IntegerOperations() {
+		Spreadsheet n = new Spreadsheet();
+		
+		n.set("A2", "=A1");
+		n.set("A1", "=A2");
+		
+		assertEquals("Evaluation is not correct", "#Circular", n.evaluate("A1"));
+	}
+	
+	
 	
 
 }
