444fd831c6b85101afc5969013bc8c99a9b46e55
Olli Hiekkaranta <olli.hiekkaranta@gmail.com>
2016-10-17 11:45:30 +0300

    add some logic and test

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,18 +1,136 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Spreadsheet {
+	
+	private static final String error = "#Error";
+	private static final String circular = "#Circular";
+	
+	private HashMap<String, String> spreadSheet = new HashMap<String, String>();
+	private ArrayList<String> nodes = new ArrayList<String>();
 
 	public String get(String cell) {
-		// to be implemented
-		return null;
+		String val = spreadSheet.get(cell);
+		if (val != null) {
+			if (isValidString(val)) {
+				val = stripStringTags(val);
+			}
+			else if (isFormula(val)) {
+				val = handleFormula(cell, val);
+			}
+		}
+		else if (val != circular) {
+			nodes.remove(cell);
+		}
+		return val;
+	}
+
+	private String handleFormula(String cell, String val) {
+		if (isValidCalculation(val.substring(1))) {
+			System.err.println("calc");
+			val = calculate(val.substring(1));
+		}
+		else if (isReference(val.substring(1))) {
+			if (!nodes.contains(cell)) {
+				nodes.add(cell);
+			}
+			else {
+				val = circular;
+			}
+		}
+		if (val != circular) {
+			val = evaluate(val.substring(1));
+		}
+		return val;
+	}
+
+	private boolean isFormula(String val) {
+		if (val.startsWith("=")) {
+			return true;
+		}
+		return false;
+	}
+
+	private String stripStringTags(String val) {
+		val = val.replace("'", "");
+		return val;
 	}
 	
 	public void set(String cell, String value) {
-		// to be implemented
+		if (isValidInteger(value) || isValidString(value) || isFormula(value) || isValidCalculation(value)) {
+			spreadSheet.put(cell, value);
+		}
+		else {
+			spreadSheet.put(cell, error);
+		}
 	}
 	
+	private boolean isValidString(String value) {
+		if (value.startsWith("'") && value.endsWith("'")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isValidInteger(String value) {
+		try {
+			Integer.parseInt(value);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
 	public String evaluate(String cell) {
-		// to be implemented
-		return null;
+		String val = cell;
+		if (isValidString(val)) {
+			val = stripStringTags(val);
+		}
+		else if (isValidInteger(val)) {
+			//
+		}
+		else if (isReference(val)) {
+			val = this.get(val);
+		}
+		else {
+			return error;
+		}
+		return val;
+	}
+
+	private String calculate(String cell) {
+		String[] values = cell.split("[-+*/%]");
+		int result = 0;
+		for (int i = 0; i < values.length; i++) {
+			System.err.println(values[i]);
+			result += Integer.parseInt(this.get(values[i]));
+		}
+		return Integer.toString(result);
+	}
+
+	private boolean isValidCalculation(String cell) {
+		String[] values = cell.split("[-+*/%]");
+		if (values.length > 1) {
+			for (int i = 0; i < values.length; i++) {
+				if (!isValidInteger(this.get(values[i]))) {
+					return false;
+				}
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	private boolean isReference(String cell) {
+		if (isValidInteger(cell) || isValidString(cell)) {
+			return false;
+		}
+		if (Character.isLetter(cell.charAt(0)) && Character.isDigit(cell.charAt(1))) {
+			return true;
+		}
+		return false;
 	}
 	
 }
--- tests/SpreadsheetTest.java
+++ tests/SpreadsheetTest.java
@@ -5,11 +5,103 @@ import org.junit.Test;
 
 
 public class SpreadsheetTest {
+	
+	Spreadsheet sheet = new Spreadsheet();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void SpreadsheetTest_assign_get_val() {
+		sheet.set("A1", "-1");
+		String val = sheet.get("A1");
+		assertEquals("-1", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_get_wrong_format_int() {
+		sheet.set("A1", "5A");
+		String val = sheet.get("A1");
+		assertEquals("#Error", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_get_valid_string() {
+		sheet.set("A1", "'a string'");
+		String val = sheet.get("A1");
+		assertEquals("a string", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_get_invalid_string() {
+		sheet.set("A1", "'a string");
+		String val = sheet.get("A1");
+		assertEquals("#Error", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_formula_get_valid_string() {
+		sheet.set("A1", "='a string'");
+		String val = sheet.get("A1");
+		assertEquals("a string", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_formula_get_invalid_string() {
+		sheet.set("A1", "='a string");
+		String val = sheet.get("A1");
+		assertEquals("#Error", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_formula_get_valid_reference() {
+		sheet.set("A1", "='a string'");
+		sheet.set("A2", "=A1");
+		String val = sheet.get("A2");
+		assertEquals("a string", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_int_ref_get() {
+		sheet.set("A1", "5");
+		sheet.set("A2", "=A1");
+		String val = sheet.get("A2");
+		assertEquals("5", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_assign_circular_reference_get() {
+		sheet.set("A1", "=A2");
+		sheet.set("A2", "=A1");
+		String val = sheet.get("A1");
+		assertEquals("#Circular", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_invalid_calculation() {
+		sheet.set("A1", "10");
+		sheet.set("A2", "5");
+		sheet.set("A3", "asdf");
+		sheet.set("A4", "A1+A2+A3");
+		String val = sheet.get("A4");
+		assertEquals("#Error", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_valid_calculation() {
+		sheet.set("A1", "10");
+		sheet.set("A2", "5");
+		sheet.set("A3", "4");
+		sheet.set("A4", "=A1+A2+A3");
+		String val = sheet.get("A4");
+		assertEquals("19", val);
+	}
+	
+	@Test
+	public void SpreadsheetTest_valid_calculation_with_ref() {
+		sheet.set("A1", "10");
+		sheet.set("A2", "5");
+		sheet.set("A3", "=A1");
+		sheet.set("A4", "=A1+A2+A3");
+		String val = sheet.get("A4");
+		assertEquals("25", val);
 	}
 
 }
